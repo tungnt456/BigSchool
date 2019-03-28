e668f2e6ffd2950381eb677079b5990223db2b39
Administrator <Administrator@PC010>
2019-03-28 13:38:59 +0700

    Add project files.

---
+++ Lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab456.Startup))]
+namespace Lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
